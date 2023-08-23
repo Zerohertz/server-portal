ee6bb83d475e50c8a95d24df7c85ded7f752d773
Zerohertz <ohg3417@gmail.com>
2023-08-23 10:11:15 +0900

    :rocket: Initial Commit

---
+++ src/common/constants/app.ts
@@ -0,0 +1,49 @@
+import type { AppInterface } from "@/common/types/app";
+
+export const APP_LIST: AppInterface[] = [
+  {
+    name: "GitHub",
+    href: "https://github.com/Zerohertz",
+    imageSrc: "/images/github.png",
+  },
+  {
+    name: "Blog",
+    href: "https://zerohertz.github.io",
+    imageSrc: "/images/blog.png",
+  },
+  {
+    name: "Traefik",
+    href: "https://traefik.zerohertz.xyz",
+    imageSrc: "/images/traefik.png",
+  },
+  {
+    name: "Kubernetes",
+    href: "https://k8s.zerohertz.xyz",
+    imageSrc: "/images/k8s.png",
+  },
+  {
+    name: "Prometheus",
+    href: "https://prometheus.zerohertz.xyz",
+    imageSrc: "/images/prometheus.png",
+  },
+  {
+    name: "Grafana",
+    href: "https://grafana.zerohertz.xyz",
+    imageSrc: "/images/grafana.png",
+  },
+  {
+    name: "Airflow",
+    href: "https://airflow.zerohertz.xyz",
+    imageSrc: "/images/airflow.png",
+  },
+  {
+    name: "Argo CD",
+    href: "https://argocd.zerohertz.xyz",
+    imageSrc: "/images/argocd.png",
+  },
+  {
+    name: "Nextcloud",
+    href: "https://cloud.zerohertz.xyz",
+    imageSrc: "/images/nextcloud.png",
+  },
+];
